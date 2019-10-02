6e437964112c5d96231ff75a3f6585fc210fa08d
Ricardo Ferreira da Silva <ricardoofficiall@gmail.com>
2019-10-02 21:23:40 +0100

    Connected with backend.

--- project_management/back_end/routes/users.js
+++ project_management/back_end/routes/users.js
@@ -13,10 +13,32 @@ router.route("/create").post((req, res) => {
     let password = req.body.password;
     let email = req.body.email;
 
-    let newUser = new User({ username,email })
+    let newUser = new User({ username, email })
     newUser.setPassword(password)
 
-    newUser.save().then( () => {res.json("User created sucessfully.")})
-    .catch(err => res.status(400).json("Error: " + err))
+    newUser.save().then(() => { res.json("User created sucessfully.") })
+        .catch(err => res.status(400).json("Error: " + err))
 });
+
+router.route("/login").post((req, res) => {
+    let name = req.body.username;
+    let password = req.body.password;
+
+    let user = User.findOne({ username: name }, (err, user) => {
+        if (err) res.status(400).json("Error: " + err);
+        else {
+
+            if (user.validPassword(password)) {
+                res.send({ token: "yourtoken" });
+            }
+            else {
+                res.status(400).json("Authentication error.");
+            }
+        }
+    })
+
+});
+
+
+
 module.exports = router
